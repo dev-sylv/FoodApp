e2ef77b16f0b442d0d21f916bf9d7584204099e4
nicsylvia <nicsylvia15f@gmail.com>
2023-05-05 01:11:21 -0700

    account verified ui

--- src/pages/Auth/RedirectToEmail.tsx
+++ src/pages/Auth/RedirectToEmail.tsx
@@ -2,16 +2,21 @@ import React from "react";
 
 const RedirectToEmail = () => {
   return (
-    <div className="w-full h-screen bg-orange-500 flex justify-center items-center">
-      <div className="w-3/5 flex justify-center items-center shadow-2xl bg-white h-64">
-        <div className="w-3/5 flex items-center bg-red-300 flex-col h-64">
-          <h1 className="">Food King</h1>
-          <h3 className="mt-5 ">Please Confirm your Email 💯🔴</h3>
+    <div className="w-full h-screen  flex justify-center items-center">
+      <div className="w-2/5 flex justify-center items-center shadow-2xl bg-white h-72">
+        <div className="w-4/5 flex items-center flex-col h-64">
+          <h1 className="text-green-500">
+            Food <span className="text-orange-500">King</span>
+          </h1>
+          <h3 className="mt-5 text-2xl text-green-500">
+            Please Confirm your Email 💯🔴
+          </h3>
           <p className="text-center mt-5">
-            An Email has been sent to the <span>Email Address</span> you used to
+            An Email has been sent to the{" "}
+            <span className="text-orange-500">Email Address</span> you used to
             register an Account for this site, Please go check it and{" "}
-            <span>follow the Link</span> to complete your{" "}
-            <span>Registeration</span> 💙
+            <span className="text-orange-500">follow the Link</span> to complete
+            your <span className="text-orange-500">Registeration</span> 💙
           </p>
         </div>
       </div>
--- src/pages/Auth/Verification.tsx
+++ src/pages/Auth/Verification.tsx
@@ -1,7 +1,30 @@
 import React from "react";
 
 const Verification = () => {
-  return <div>Verification</div>;
+  return (
+    <div>
+      <div className="w-full h-screen  flex justify-center items-center">
+        <div className="w-2/5 flex justify-center items-center shadow-2xl bg-white h-72">
+          <div className="w-4/5 flex items-center flex-col h-64">
+            <h1 className="text-green-500">
+              Food <span className="text-orange-500">King</span>
+            </h1>
+            <h3 className="mt-5 text-2xl text-green-500">
+              Please Confirm your Email 💯🔴
+            </h3>
+            <p className="text-center mt-5">
+              An Email has been sent to the{" "}
+              <span className="text-orange-500">Email Address</span> you used to
+              register an Account for this site, Please go check it and{" "}
+              <span className="text-orange-500">follow the Link</span> to
+              complete your{" "}
+              <span className="text-orange-500">Registeration</span> 💙
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Verification;
