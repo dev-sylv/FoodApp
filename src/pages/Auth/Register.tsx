c6f3da423f6f0247ef9f76d06ce4fc83df3da22b
nicsylvia <nicsylvia15f@gmail.com>
2023-05-04 22:49:34 -0700

    working on simple register ui

--- src/pages/Auth/Register.tsx
+++ src/pages/Auth/Register.tsx
@@ -1,7 +1,46 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Register = () => {
-  return <div className="w-full h-screen bg-slate-400"></div>;
+  return (
+    <div className="w-full h-screen bg-green-500 flex justify-center items-center">
+      <div className="pd-40 flex flex-col justify-center items-center w-54 ">
+        <h4>Sign Up</h4>
+
+        <div className="w-300 bg-transparent h-40 pl-32 border-spacing-x-4 text-white">
+          <input type="text" placeholder="Enter your name" />
+        </div>
+
+        <div className="w-300 bg-transparent h-40 pl-32 border-spacing-x-4 text-white">
+          <input type="email" placeholder="Enter a valid email" />
+        </div>
+
+        <div className="w-300 bg-transparent h-40 pl-32 border-spacing-x-4 text-white">
+          <input
+            type="password"
+            placeholder="Enter a password you can remember"
+          />
+        </div>
+
+        <div className="w-300 bg-transparent h-40 pl-32 border-spacing-x-4 text-white">
+          <input type="password" placeholder="Confirm Password" />
+        </div>
+
+        <div className="bg-orange-500 border-spacing-0 w-320 border-r-4 h-40 text-white font-bold">
+          <button>Sign up</button>
+        </div>
+
+        <div className="flex justify-center items-center">
+          <p className="text-white font-semibold mr-9">
+            Already have an account?
+          </p>
+          <NavLink to="/login-user">
+            <b className="text-white cursor-pointer">Log in here</b>
+          </NavLink>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
