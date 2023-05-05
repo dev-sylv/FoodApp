f225f2791f5267f5a4168111646b1833ccca57d6
nicsylvia <nicsylvia15f@gmail.com>
2023-05-05 01:03:44 -0700

    working on redirect to email ui

--- src/pages/Auth/RedirectToEmail.tsx
+++ src/pages/Auth/RedirectToEmail.tsx
@@ -1,7 +1,22 @@
 import React from "react";
 
 const RedirectToEmail = () => {
-  return <div>RedirectToEmail</div>;
+  return (
+    <div className="w-full h-screen bg-orange-500 flex justify-center items-center">
+      <div className="w-3/5 flex justify-center items-center shadow-2xl bg-white h-64">
+        <div className="w-3/5 flex items-center bg-red-300 flex-col h-64">
+          <h1 className="">Food King</h1>
+          <h3 className="mt-5 ">Please Confirm your Email 💯🔴</h3>
+          <p className="text-center mt-5">
+            An Email has been sent to the <span>Email Address</span> you used to
+            register an Account for this site, Please go check it and{" "}
+            <span>follow the Link</span> to complete your{" "}
+            <span>Registeration</span> 💙
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default RedirectToEmail;
