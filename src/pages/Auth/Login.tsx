617cca6dbd2013f1a7b4e49b8fd9148a7570bc93
nicsylvia <nicsylvia15f@gmail.com>
2023-05-05 00:33:00 -0700

    saving forgot password

--- src/pages/Auth/Login.tsx
+++ src/pages/Auth/Login.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Login = () => {
   return (
@@ -6,9 +7,9 @@ const Login = () => {
       <div className="border shadow-2xl py-10 flex flex-col justify-center items-center w-6/12">
         <h4 className="text-white font-bold text-4xl mb-5">Login</h4>
         <div className="w-full max-w-xs">
-          <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+          <form className=" ">
             <div className="mb-4">
-              <label className="block text-gray-700 text-sm font-bold mb-2">
+              <label className="block text-white text-sm font-bold mb-2">
                 Email
               </label>
               <input
@@ -18,17 +19,17 @@ const Login = () => {
               />
             </div>
             <div className="mb-6">
-              <label className="block text-gray-700 text-sm font-bold mb-2">
+              <label className="block text-white text-sm font-bold mb-2">
                 Password
               </label>
               <input
-                className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
                 type="password"
-                placeholder="******************"
+                placeholder="Enter your password"
               />
-              <p className="text-red-500 text-xs italic">
+              {/* <p className="text-red-500 text-xs italic">
                 Please enter your password.
-              </p>
+              </p> */}
             </div>
             <div className="flex items-center justify-between">
               <button
@@ -37,14 +38,18 @@ const Login = () => {
                 Sign In
               </button>
               <a
-                className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
+                className="inline-block align-baseline font-bold text-base text-orange-500 hover:text-blue-800"
                 href="#">
-                Forgot Password?
+                Forgot Password?{" "}
+                <span className="text-white cursor-pointer">click here</span>
               </a>
             </div>
           </form>
-          <p className="text-center text-gray-500 text-xs">
-            Don't have an account? <span>Sign up here</span>
+          <p className="text-center text-white mt-5 text-lg">
+            Don't have an account?{" "}
+            <NavLink to="/register-user">
+              <span className="cursor-pointer">Sign up here</span>
+            </NavLink>
           </p>
         </div>
       </div>
--- src/pages/Auth/Register.tsx
+++ src/pages/Auth/Register.tsx
@@ -4,40 +4,73 @@ import { NavLink } from "react-router-dom";
 const Register = () => {
   return (
     <div className="w-full h-screen bg-green-500 flex justify-center items-center">
-      <div className="border shadow-2xl py-10 flex flex-col justify-center items-center w-6/12 ">
-        <h4 className="text-white font-bold text-4xl">Sign Up</h4>
+      <div className="border shadow-2xl py-10 flex flex-col justify-center items-center w-6/12">
+        <h4 className="text-white font-bold text-4xl mb-5">Sign Up</h4>
+        <div className="w-full max-w-xs">
+          <form className=" ">
+            <div className="mb-4">
+              <label className="block text-white text-sm font-bold mb-2">
+                Name
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                type="email"
+                placeholder="Enter your name"
+              />
+            </div>
+            <div className="mb-4">
+              <label className="block text-white text-sm font-bold mb-2">
+                Email
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                type="email"
+                placeholder="Enter your email"
+              />
+            </div>
 
-        <div className="w-80 outline-none rounded-xl h-10 bg-transparent px-5 border-2 text-white">
-          <input type="text" placeholder="Enter your name" />
-        </div>
+            <div className="mb-6">
+              <label className="block text-white text-sm font-bold mb-2">
+                Password
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                type="password"
+                placeholder="Enter your password"
+              />
+              {/* <p className="text-red-500 text-xs italic">
+                Please enter your password.
+              </p> */}
+            </div>
 
-        {/* <div className="w-300 bg-transparent h-40 pl-32 border-spacing-x-4 text-white">
-          <input type="email" placeholder="Enter a valid email" />
-        </div>
-
-        <div className="w-300 bg-transparent h-40 pl-32 border-spacing-x-4 text-white">
-          <input
-            type="password"
-            placeholder="Enter a password you can remember"
-          />
-        </div>
-
-        <div className="w-300 bg-transparent h-40 pl-32 border-spacing-x-4 text-white">
-          <input type="password" placeholder="Confirm Password" />
-        </div>
-
-        <div className="bg-orange-500 border-spacing-0 w-320 border-r-4 h-40 text-white font-bold">
-          <button>Sign up</button>
-        </div>
-
-        <div className="flex justify-center items-center">
-          <p className="text-white font-semibold mr-9">
-            Already have an account?
+            <div className="mb-6">
+              <label className="block text-white text-sm font-bold mb-2">
+                Confirm Password
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                type="password"
+                placeholder="Confirm your password"
+              />
+              {/* <p className="text-red-500 text-xs italic">
+                Please enter your password.
+              </p> */}
+            </div>
+            <div className="flex items-center justify-between">
+              <button
+                className=" w-full bg-orange-500 hover:bg-orange-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                type="button">
+                Sign In
+              </button>
+            </div>
+          </form>
+          <p className="text-center text-white mt-5 text-lg">
+            Already have an account?{" "}
+            <NavLink to="/login-user">
+              <span className="cursor-pointer">Login here</span>
+            </NavLink>
           </p>
-          <NavLink to="/login-user">
-            <b className="text-white cursor-pointer">Log in here</b>
-          </NavLink>
-        </div> */}
+        </div>
       </div>
     </div>
   );
